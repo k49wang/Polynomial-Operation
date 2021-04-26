4d301a94e2cdf5870784a33a1ce7807b7b451cc2
Kai <k49wang@uwaterloo.ca>
2021-04-26 18:25:49 -0400

    updated main

--- main.cc
+++ main.cc
@@ -6,8 +6,19 @@
 int main( int argc, char* argv[] ) {
     Polynomial poly1, poly2;
     std::cin >> poly1 >> poly2;
+    std::cout << "First Polynomial: \t";
     std::cout << poly1 << std::endl;
+    std::cout << "Second Polynomial: \t";
     std::cout << poly2 << std::endl;
+
+    std::cout << "Addition: \t\t";
+    std::cout << poly1 + poly2 << std::endl;
+    std::cout << "Subtraction: \t\t";
+    std::cout << poly1 - poly2 << std::endl;
+    std::cout << "Multiplication: \t";
+    std::cout << poly1 * poly2 << std::endl; 
+    std::cout << "Division: \t\t";
     std::cout << poly1 / poly2 << std::endl; 
+    std::cout << "Mudulo: \t\t";
     std::cout << poly1 % poly2 << std::endl; 
 }
